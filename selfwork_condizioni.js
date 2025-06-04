c3d5c5a8275f9e87704d7aa18372cb31f26b4383
Vincenzo Rosa <vincenzo.rosa99@gmail.com>
2025-06-04 17:27:50 +0200

    selfwork consizioni 2

--- selfwork_condizioni.js
+++ selfwork_condizioni.js
@@ -78,3 +78,85 @@ Esempio:
 let v = 29;
 Output: Ottimo
 Cercate di risolvere questo esercizio utilizzando prima if, else e poi con switch. */
+
+
+
+
+
+
+
+let temperatura_1 = prompt (`Quanti gradi fanno??`);
+
+console.log(temperatura_1);
+
+
+if ( temperatura_1 >= 0 && temperatura_1 < 20) {
+    console.log(`non ci sono piu’ le mezze stagioni`);
+
+} else if (temperatura_1 >= 30) {
+    console.log(`u mare, lu sole, lu ientu`);
+
+} else if (temperatura_1 >= 20 && temperatura_1 < 30) {
+    console.log(`mi dia una peroni sudata`);
+
+} else if (temperatura_1 >= -10 && temperatura_1 < 0) {
+    console.log(`non e’ tanto il freddo quanto l’umidità`);
+
+} else if (temperatura_1 < -10) {
+    console.log(`copriti…ancora ti raffreddi`);
+
+} 
+
+/*
+Scrivi un programma che converta una temperatura inserita dall’utente con una delle seguenti descrizioni, stampate in console:
+se temperatura e’ minore di 20, stampare “non ci sono piu’ le mezze stagioni”
+se temperatura e’ maggiore uguale a 30, stampare “lu mare, lu sole, lu ientu”
+se temperatura e’ minore di 30, stampare “mi dia una peroni sudata”
+se temperatura e’ minore di 0, stampare “non e’ tanto il freddo quanto l’umidità’”
+se temperatura e’ minore di -10, stampare “copriti…ancora ti raffreddi”
+Cerca di risolvere questo esercizio utilizzando prima if else e poi con switch case. */
+
+
+
+let temperatura_2  = prompt `Inserisci la seconda temperatura`
+console.log(temperatura_2);
+
+
+switch (true) {
+    case ( temperatura_2 >= 0 && temperatura_2 < 20):
+        console.log('non ci sono piu’ le mezze stagioni');
+        break;
+    
+    case ( temperatura_2 >= 30):
+        console.log(`lu mare, lu sole, lu ientu`);
+        break;
+
+    case (temperatura_2 >= 20 && temperatura_2 < 30 ):
+        console.log(`mi dia una peroni sudata`);
+        break;
+
+    case (temperatura_2 >= -10 && temperatura_2 <0):
+        console.log(`non e’ tanto il freddo quanto l’umidità`);
+        break;
+
+    case (temperatura_2 < -10):
+        console.log(`copriti…ancora ti raffreddi`);
+        break;
+
+    default:
+        console.log(`Non sono ammessi alti caratteri se non i numeri`);
+        
+}
+
+
+
+
+
+/*
+Scrivi un programma che converta una temperatura inserita dall’utente con una delle seguenti descrizioni, stampate in console:
+se temperatura e’ minore di 20, stampare “non ci sono piu’ le mezze stagioni”
+se temperatura e’ maggiore uguale a 30, stampare “lu mare, lu sole, lu ientu”
+se temperatura e’ minore di 30, stampare “mi dia una peroni sudata”
+se temperatura e’ minore di 0, stampare “non e’ tanto il freddo quanto l’umidità’”
+se temperatura e’ minore di -10, stampare “copriti…ancora ti raffreddi”
+Cerca di risolvere questo esercizio utilizzando prima if else e poi con switch case. */
